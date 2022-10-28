d617e8a733e4ca4862a4fb538c5558fcb7342f81
Mr. Hernandez <mr.hernandez@SLA-vh.local>
2022-10-28 13:48:59 -0400

    Initial commit

---
+++ src/TakeAttendance.java
@@ -0,0 +1,7 @@
+public class TakeAttendance {
+    public static void main(String[] args) {
+
+        System.out.println("Hi class.  Who's here?");
+        System.out.println("Mr Hernandez, says: I'm here.");
+    }
+}
