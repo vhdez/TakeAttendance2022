a6b3f6d38cc3657e41d88b1422e21e67addc22dc
sjohnsonn <sjohnson24@scienceleasdersjip.org>
2022-10-31 14:53:18 -0400

    Siraj is here

--- src/TakeAttendance.java
+++ src/TakeAttendance.java
@@ -3,10 +3,12 @@ public class TakeAttendance {
 
         System.out.println("Hi class.  Who's here?");
         System.out.println("Mr Hernandez, says: I'm here.");
-        System.out.println("Elijah, says: I'm here.");
         System.out.println("It's 2pm.  Please say hi to be marked present.");
         System.out.println("Riley, says: I'm here.");
         System.out.println("Gia, says: I'm here.");
         System.out.println("Chris, says: I'm here.");
+        System.out.println("Siraj, says: I'm here.");
+
+
     }
 }
