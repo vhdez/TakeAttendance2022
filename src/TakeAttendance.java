cdcbf14076c37b90a802e67288d788e2dfbe6b3f
vhdez <vhernandez@scienceleadership.org>
2022-10-31 14:08:02 -0400

    I added 2pm info.

--- src/TakeAttendance.java
+++ src/TakeAttendance.java
@@ -3,5 +3,6 @@ public class TakeAttendance {
 
         System.out.println("Hi class.  Who's here?");
         System.out.println("Mr Hernandez, says: I'm here.");
+        System.out.println("It's 2pm.  Please say hi to be marked present.");
     }
 }
