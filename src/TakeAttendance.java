959dec8f0c755fc2ee5649495a0ed69beffd9d94
ElijahGoat326 <elijahlopez326@gmail.com>
2022-11-03 09:47:53 -0400

    I said "i'm here" after owen

--- src/TakeAttendance.java
+++ src/TakeAttendance.java
@@ -6,7 +6,6 @@ public class TakeAttendance {
         System.out.println("It's 2pm.  Please say hi to be marked present.");
         System.out.println("Riley, says: I'm here.");
         System.out.println("Gia, says: I'm here.");
-        System.out.println("Owen, really says:I'm here.");
         System.out.println("Elijah, says: I'm here.");
         System.out.println("Owen, really says:I'm here.");
         System.out.println("Chris, says: I'm here.");
